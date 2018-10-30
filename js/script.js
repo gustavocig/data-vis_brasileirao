43ad06bf092944660a476ba8a34075bdaf947fe1
GustavoCig <gustavocignachi@hotmail.com>
2018-10-30 16:45:35 -0300

    Improves code and adds zoom constraints, dragging constraints might be over complicating the issue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-function create_map(html_tag, lat, long, magnification) {
-	let map = L.map(html_tag).setView([lat, long], magnification);
-            L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+function create_map(html_tag, lat, long, magnification, options) {
+  let options_is_set = typeof options != 'undefined'
+                        && options != null
+  // If options object isnt set, uses default config
+  options = options_is_set ? options : { minZoom: magnification };
+
+	let map = L.map(html_tag, options).setView([lat, long], magnification);
+  L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
             { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
             maxZoom: 18}).addTo(map);
 }
