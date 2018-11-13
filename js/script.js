d365ba6fd17f147fb34244311b71686d354ee2dc
GustavoCig <gustavocignachi@hotmail.com>
2018-11-13 20:39:33 -0300

    Improves view's layout

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ function create_map(html_tag, lat, long, magnification, options) {
   let options_is_set = typeof options != 'undefined'
                         && options != null
   // If options object isnt set, uses default config
-  options = options_is_set ? options : { minZoom: magnification };
+  options = options_is_set ? options : {
+    minZoom: magnification,
+    maxZoom: magnification,
+    dragging: false,
+    zoomControl: false,
+  };
 
 	let map = L.map(html_tag, options).setView([lat, long], magnification);
   L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
