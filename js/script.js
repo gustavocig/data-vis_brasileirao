a50910181a2333278f2112cfd5e05d243026926f
Antonio Diogo Forte Martins <antdiogofmartins@gmail.com>
2018-11-13 22:49:47 -0300

    Dataset standings per round

--- js/script.js
+++ js/script.js
@@ -14,3 +14,43 @@ function create_map(html_tag, lat, long, magnification, options) {
             { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
             maxZoom: 18}).addTo(map);
 }
+
+// Test pending
+function create_standings_series_chart(html_tag, width, height, y_dimension, group, data){
+	let standing_line_chart = dc.seriesChart(html_tag);
+
+	standing_line_chart.width(width)
+					   .height(height)
+					   .chart(function(c) { return dc.lineChart(c).curve(d3.curveCardinal); })
+					   .x(d3.scaleLinear().domain([1,38]))
+					   .brushOn(false)
+					   .yAxisLabel("Posição")
+					   .xAxisLabel("Rodada")
+					   .clipPadding(10)
+					   .elasticY(true)
+					   .dimension(y_dimension)
+					   .group(group)
+					   .mouseZoomable(true)
+					   .seriesAccessor(function(d) {return d.key[0];})
+					   .keyAccessor(function(d) {return +d.key[1];})
+					   .valueAccessor(function(d) {return +d.value;})
+					   .legend(dc.legend().x(700).y(5).itemHeight(13).gap(5));
+
+	return standing_line_chart;
+}
+
+// Test pending
+function create_goals_bar_chart(html_tag, width, height, y_dimension, group, teams_ordered, data){
+	let goals_bar_chart = dc.barChart(html_tag);
+
+	goals_bar_chart.width(width)
+	                .height(height)
+	                .margins({top: 20, right: 50, bottom: 20, left: 40})
+	            	.x(d3.scaleOrdinal().domain(teams_ordered))
+	                .xUnits(dc.units.ordinal)
+	                .barPadding(0.4)
+	                .dimension(y_dimension)
+	                .group(group)
+
+	return goals_bar_chart;
+}
