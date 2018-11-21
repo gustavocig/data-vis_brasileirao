47856b3accac833ad5361374656f794becbf04df
GustavoCig <gustavocignachi@hotmail.com>
2018-11-20 21:16:59 -0300

    Fixes identation

--- js/script.js
+++ js/script.js
@@ -37,22 +37,23 @@ function superimpose_geojson(dataset, map) {
 function create_standings_series_chart(html_tag, width, height, y_dimension, group, data){
 	let standing_line_chart = dc.seriesChart(html_tag);
 
-	standing_line_chart.width(width)
-					   .height(height)
-					   .chart(function(c) { return dc.lineChart(c).curve(d3.curveCardinal); })
-					   .x(d3.scaleLinear().domain([1,38]))
-					   .brushOn(false)
-					   .yAxisLabel("Posição")
-					   .xAxisLabel("Rodada")
-					   .clipPadding(10)
-					   .elasticY(true)
-					   .dimension(y_dimension)
-					   .group(group)
-					   .mouseZoomable(true)
-					   .seriesAccessor(function(d) {return d.key[0];})
-					   .keyAccessor(function(d) {return +d.key[1];})
-					   .valueAccessor(function(d) {return +d.value;})
-					   .legend(dc.legend().x(700).y(5).itemHeight(13).gap(5));
+	standing_line_chart
+        .width(width)
+		.height(height)
+		.chart(function(c) { return dc.lineChart(c).curve(d3.curveCardinal); })
+		.x(d3.scaleLinear().domain([1,38]))
+		.brushOn(false)
+		.yAxisLabel("Posição")
+		.xAxisLabel("Rodada")
+		.clipPadding(10)
+		.elasticY(true)
+		.dimension(y_dimension)
+		.group(group)
+		.mouseZoomable(true)
+		.seriesAccessor(function(d) {return d.key[0];})
+		.keyAccessor(function(d) {return +d.key[1];})
+		.valueAccessor(function(d) {return +d.value;})
+		.legend(dc.legend().x(700).y(5).itemHeight(13).gap(5));
 
 	return standing_line_chart;
 }
@@ -61,14 +62,15 @@ function create_standings_series_chart(html_tag, width, height, y_dimension, gro
 function create_goals_bar_chart(html_tag, width, height, y_dimension, group, teams_ordered, data){
 	let goals_bar_chart = dc.barChart(html_tag);
 
-	goals_bar_chart.width(width)
-	                .height(height)
-	                .margins({top: 20, right: 50, bottom: 20, left: 40})
-	            	.x(d3.scaleOrdinal().domain(teams_ordered))
-	                .xUnits(dc.units.ordinal)
-	                .barPadding(0.4)
-	                .dimension(y_dimension)
-	                .group(group)
+	goals_bar_chart
+        .width(width)
+	    .height(height)
+	    .margins({top: 20, right: 50, bottom: 20, left: 40})
+	    .x(d3.scaleOrdinal().domain(teams_ordered))
+	    .xUnits(dc.units.ordinal)
+	    .barPadding(0.4)
+	    .dimension(y_dimension)
+	    .group(group)
 
 	return goals_bar_chart;
 }
